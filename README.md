dc582b399411e35f846750d894599103a6225e50
Mark Engelberg <mark.engelberg@gmail.com>
2018-07-29 12:01:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# mister-rogers
+
+Mister Rogers is currently a work-in-progress. The aim is to implement local neighborhood search algorithms in Clojure. The protocols are inspired by the JAMES framework in Java. At this time, I am undecided whether to implement this as native Clojure, or whether to wrap JAMES functionality. I will probably explore and benchmark both approaches before deciding.
+
+## Usage
+
+Don't use yet.
+
+## License
+
+Copyright © 2018 Mark Engelberg
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject mister-rogers "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 ;; General-purpose tooling
+                 [better-cond "2.0.1-SNAPSHOT"]
+                 [medley "1.0.0"]
+                 [com.rpl/specter "1.1.1"]
+                 ;; Improved data structures
+                 [org.clojure/data.int-map "0.2.4"]
+                 ;; For simpler API namespacing
+                 [potemkin "0.4.5"]
+                 ;; For Java version
+                 [org.jamesframework/james-core "1.2"]
+                 [org.jamesframework/james-extensions "1.2"]
+                 ;; For writing examples
+                 [org.clojure/data.generators "0.1.2"]
+                 [semantic-csv "0.1.0"]
+                 [clojure-csv/clojure-csv "2.0.1"]]
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  :plugins [[lein-virgil "0.1.8"]])
+  
+                 
+
