010911d67c9b455e6d8749f591e932623831d429
Mark Engelberg <mark.engelberg@gmail.com>
2018-08-04 20:19:14 -0700

    Rewrote, breaking up state

--- src/clj/mister_rogers/search.clj
+++ src/clj/mister_rogers/search.clj
@@ -12,89 +12,50 @@
             [mister-rogers.problem :as prob]
             [mister-rogers.stop-criterion-checker :as crit]))
 
+(declare init start stop search-started search-stopped search-disposed search-step
+         compute-delta)
+
 (def next-id (atom 0))
 (defn get-next-id [] (swap! next-id inc))
 
-(defrecord SearchState [^long start-time, ^long stop-time,
-                        ^long current-steps, ^long last-improvement-time,
-                        ^long steps-since-last-improvement,
-                        ^boolean improvement-during-current-step?,
-                        ^double min-delta,
-                        best-solution, best-solution-evaluation,
-                        best-solution-validation, status])
+(defrecord Timestamps [^long start-time ^long stop-time
+                       ^long last-improvement-time])
 
-;; The last field in the record, status, is one of either:
-;; :idle, :initializing, :running, :terminating, :disposed
+(defrecord SEV [solution evaluation validation])
+
+(defrecord StepInfo [^long current-steps ^long steps-since-last-improvement])
+
 ;; Primitives are initialized to -1
 
 (defn label [search]
   (str \( (:name search) \: (:id search) \)))
-(declare start) ;; To implement runnable interface
-(defrecord Search [name id problem search-listeners stop-criterion-checker state]
+
+(defrecord Search [name id problem search-listeners stop-criterion-checker
+                   a-timestamps a-best a-current a-step-info a-min-delta v-status]
   Runnable
   (run [this] (start this))
   Object
   (toString [this] (label this)))
 
-;; Search's state is an atom containing a SearchState
-
-;; SearchState getters for convenience
-(defn ^long get-start-time [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-start-time search-state)))
-
-(defn ^long get-stop-time [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-stop-time search-state)))
-
-(defn ^long get-current-steps [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-current-steps search-state)))
-
-(defn ^long get-last-improvement-time [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-last-improvement-time search-state)))
-
-(defn ^long get-steps-since-last-improvement [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-steps-since-last-improvement search-state)))
-
-(defn ^boolean get-improvement-during-current-step? [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-improvement-during-current-step? search-state)))
-
-(defn ^double get-min-delta [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-min-delta search-state)))
-
-(defn get-best-solution [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-best-solution search-state)))
-
-(defn get-best-solution-evaluation [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-best-solution-evaluation search-state)))
-
-(defn get-best-solution-validation [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-best-solution-validation search-state)))
-
-(defn get-status [^Search search]
-  (let [^SearchState search-state @(.-state search)]
-    (.-status search-state)))
+;; v-status is a volatile containing one of
+;; :idle, :initializing, :running, :terminating, :disposed
+;; Changes to v-status are generally associated with a
+;; triggering of listeners that can potentially have side effects, so
+;; neither atoms nor refs will suffice.
+;; Therefore, always lock v-status before changing.
 
 (defnc search
   "Takes a map with required key :problem and optional keys
 :name, :search-listeners, :stop-criterion-checker"
   [init-map]
-  :let [state {:start-time -1, :stop-time -1, :current-steps -1,
-               :last-improvement-time -1, :steps-since-last-improvement -1,
-               :improvement-during-current-step? false
-               :min-delta -1.0, :status :idle}
-        id (get-next-id)
-        default-map {:name "Search"
-                     :id id
-                     :state (atom (map->SearchState state))}]
+  :let [default-map {:name "Search"
+                     :id (get-next-id)
+                     :a-timestamps (atom (Timestamps. -1 -1 -1)),
+                     :a-best (atom (SEV. nil nil nil))
+                     :a-current nil ;; basic search doesn't track current solution
+                     :a-step-info (atom (StepInfo. -1 -1))
+                     :a-min-delta (atom 1.0)
+                     :v-status (volatile! :idle)}]
   (nil? (:problem init-map))
   (throw (ex-info ":problem is required key for init-map in search" init-map)),
   :let [search (map->Search (merge default-map init-map))]
@@ -124,50 +85,54 @@
   (doseq [{:keys [search-stopped]} search-listeners]
     (when search-stopped (search-stopped search))))
 
-(defnc fire-new-best-solution
+(defn fire-new-best-solution
   [{:keys [search-listeners] :as ^Search search}
-   best-solution best-solution-evaluation best-solution-validation]
+   best-solution best-evaluation best-validation]
   (doseq [{:keys [new-best-solution]} search-listeners]
     (when new-best-solution 
       (new-best-solution
-       search best-solution best-solution-evaluation best-solution-validation))))
+       search best-solution best-evaluation best-validation))))
 
-(defnc fire-step-completed [{:keys [search-listeners] :as search}
-                            ^long current-steps]
+(defn fire-step-completed [{:keys [search-listeners] :as search}
+                           ^long current-steps]
   (doseq [{:keys [step-completed]} search-listeners]
     (when step-completed
       (step-completed current-steps))))
 
 (defn fire-status-changed [{:keys [search-listeners] :as search} new-status]
-  (when status-changed
-    (doseq [{:keys [status-changed]} search-listeners] (status-changed new-status))))
+  (doseq [{:keys [status-changed]} search-listeners]
+    (when status-changed (status-changed new-status))))
 
 ;; Status helpers
+
+(defn get-status [search]
+  @(:v-status search))
 
 (defn status-one-of? [search status-set]
   (contains? status-set (get-status search)))
 
 (defn assert-status [search status-set msg]
   (when-not (status-one-of? search status-set)
-    (let [info (ex-info msg {:status-should-be-one-of status-set,
-                             :status (get-status search)})]
-      (throw info))))
+    (throw (ex-info msg {:status-should-be-one-of status-set,
+                         :status (get-status search)}))))
 
-(defn change-status! [{:keys [state] :as search} status]
-  (let [previous-status (get-status search)]
-    (swap! state assoc :status status)
-    (debugf "Search %s changed status %s ---> %s" search previous-status status)
-    (fire-status-changed search status)))
+(defn change-status! [{:keys [v-status] :as search} status]
+  (locking v-status
+    (let [previous-status (get-status search)]
+      (vreset! v-status status)
+      (debugf "Search %s changed status %s ---> %s" search previous-status status)
+      (fire-status-changed search status))))
 
 (defn continue-search? [search]
   (not= (get-status search) :terminating))
 
 ;; Search control functions
-(declare search-started search-stopped search-disposed search-step stop)
 
-(defn start [{:keys [stop-criterion-checker] :as ^Search search}]
-  (assert-status search #{:idle} "Cannot start search")
-  (change-status! search :initializing)
+(defn start [{:keys [stop-criterion-checker a-step-info v-status]
+              :as search}]
+  (locking v-status
+    (assert-status search #{:idle} "Cannot start search")
+    (change-status! search :initializing))
   (infof "Search %s started" search)  
   (fire-search-started search)
   (search-started search)  
@@ -175,42 +140,53 @@
     (crit/start-checking search stop-criterion-checker)
     (change-status! search :running)
     (while (continue-search? search)
-      ;; TBD: Improve performance
-      (let [^SearchState new-ss
-            (swap!
-             (.-state search)
-             (fn [^SearchState ss]
-               (let [improvement-during-current-step? (search-step search),
-                     current-steps (inc (.-current-steps ss))
-                     steps-since-last-improvement
-                     (if improvement-during-current-step?
-                       0 (inc (.-steps-since-last-improvement ss)))]
-                 (assoc ss :improvement-during-current-step? improvement-during-current-step?
-                        :current-steps current-steps
-                        :steps-since-last-improvement steps-since-last-improvement))))]
-        (fire-step-completed search (.-current-steps new-ss)))
+      (let [before-best @(:best search),
+            _ (search-step search),
+            after-best @(:best search),
+            improvement-during-step? (not (identical? before-best after-best)),
+            ^StepInfo step-info @a-step-info,
+            current-steps (inc (.-current-steps step-info)),
+            steps-since-last-improvement
+            (if improvement-during-step? 0
+                (inc (.-steps-since-last-improvement step-info)))]
+        (reset! step-info (StepInfo. current-steps steps-since-last-improvement))
+        (fire-step-completed search current-steps))
       (when (crit/stop-criterion-satisfied? stop-criterion-checker)
         (stop search)))
     (crit/stop-checking search stop-criterion-checker))
   (search-stopped search)
   (fire-search-stopped search)
-  (let [^SearchState s @(.-state search)]
-    (infof "Search %s stopped (runtime: %d ms, steps: %d" search
-           (.-runtime s) (.-current-steps s)))
+  (infof "Search %s stopped (runtime: %d ms, steps: %d" search
+         (get-runtime search) (:current-steps @a-step-info))
   (change-status! search :idle))
 
 (defn stop "Sets status to terminating to interrupt search" [search]
-  (when (status-one-of? search #{:initializing :running})
-    (change-status! search :terminating)))
+  (locking (:v-status search)
+    (when (status-one-of? search #{:initializing :running})
+      (change-status! search :terminating))))
 
 (defn dispose "Sets status to dispose so it can't be restarted" [search]
-  (when-not (status-one-of? search #{:disposed})
-    (change-status! search :disposed)))
+  (locking (:v-status search)
+    (when-not (status-one-of? search #{:disposed})
+      (assert-idle search "Cannot dispose search.")
+      (change-status! search :disposed))))
 
 ;; Updating solution
 
-(declare compute-delta)
-(defnc update-best-solution "Updates soution if better"
+(defn update-improvement-time [a-timestamps]
+  (swap! a-timestamps
+         (fn [^Timestamps timestamps]
+           (let [start-time (.-start-time timestamps),
+                 stop-time (.-stop-time timestamps)]
+             (Timestamps. start-time stop-time (System/currentTimeMillis))))))
+
+(defn update-min-delta [a-min-delta]
+  (swap! a-min-delta
+         (fn [^double min-delta]
+           (if (or (= min-delta -1.0) (< delta min-delta))
+             delta min-delta))))
+
+(defnc update-best-solution "Updates solution if better, returns true if updated"
   ([^Search search new-solution]
    :let [problem (.-problem search),
          new-validation (prob/validate problem new-solution)]
@@ -220,99 +196,90 @@
   
   ([^Search search new-solution new-evaluation new-validation]
    (not (mrp/passed? new-validation)) nil ;; invalid solution
-   :let [state (.-state search),
-         [old new]
-         (swap-vals!
-          state
-          (fn [^SearchState ss]
-            (cond
-              :let [best-solution (.-solution ss),
-                    best-solution-evaluation (.-best-solution-evaluation ss)
-                    delta (compute-delta search
-                                         new-evaluation best-solution-evaluation)]
-              (not (or (nil? best-solution) (> delta 0.0))) ss ;; no improvement
-              :let [min-delta (.-min-delta ss)
-                    new-delta (if (or (= min-delta -1.0) (< delta min-delta))
-                                delta min-delta)]
-              ;; TBD: Improve performance
-              (assoc ss :min-delta new-delta,
-                     :last-improvement-time (System/currentTimeMillis),
-                     :improvement-during-current-step? true,
-                     :best-solution new-solution
-                     :best-solution-evaluation new-evaluation
-                     :best-solution-validation new-validation))))]
-   (identical? old new) false
-   :do (fire-new-best-solution search new-solution new-evaluation new-validation)
-   true))
+   (let [a-best (.-a-best search)]
+     (swap! a-best
+            (fn [^SEV best]
+              (cond
+                :let [best-solution (.-solution best),
+                      best-evaluation (.-evaluation best),
+                      delta (compute-delta search new-evaluation best-evaluation)]
+                (not (or (nil? best-solution) (> delta 0.0))) best ;; no improve
+                :do (update-min-delta (.-a-min-delta search))                
+                :do (update-improvement-time (.-a-timestamps search))
+                (SEV. new-solution new-evaluation new-validation))))
 
-;; Helper functions
+     (fire-new-best-solution search new-solution new-evaluation new-validation)
+     true)))
+
+;; Helper computation functions
 
 (defn get-runtime
-  "If search is running or terminating, returns runtime since beginning of run in ms.
-   If search is idle or disposed, returns runtime of last run in ms, or -1 if no run yet.
+  "If search is running or terminating, returns millis since beginning of run.
+   If search is idle or disposed, returns millis of last run, or -1 if no run yet.
    If search is initializing, returns -1."
-  ^long [^SearchState ss]
-  (cond
-    :let [status (.-status ss)
-          stop-time (.-stop-time ss)
-          start-time (.-start-time ss)]
-    (= status :initializing) -1
-    (or (= status :idle) (= status :disposed)) (if (= stop-time -1) -1 (- stop-time start-time))
-    :else (- (System/currentTimeMillis) start-time)))
+  ^long [{:keys [a-timestamps v-status] :as search}]
+  (locking v-status
+    (cond
+      :let [status @v-status]      
+      (= status :intializing) -1
+      :let [^Timestamps timestamps @a-timestamps
+            start-time (.-start-time timestamps)
+            stop-time (.-stop-time timestamps)]
+      (or (= status :idle) (= status :disposed))
+      (if (= stop-time -1) -1 (- stop-time start-time)),
+      :else (- (System/currentTimeMillis) start-time))))
 
-(defn get-time-without-improvement ^long [^SearchState ss]
-  (cond
-    :let [status (.-status ss)
-          last-improvement-time (.-last-improvement-time ss)
-          stop-time (.-stop-time ss)]
-    (= status :intializing) -1
-    (= last-improvement-time -1) (get-runtime ss)
-    (or (= status :idle) (= status :disposed)) (- stop-time last-improvement-time)
-    :else (- (System/currentTimeMillis) last-improvement-time)))
+(defn get-time-without-improvement
+  "If search is running or terminating, returns runtime since last improvement.
+   If search is idle or disposed, returns stop-time - last-improvement-time.
+   If search is initializing, returns -1"  
+  ^long [{:keys [a-timestamps v-status] :as search}]
+  (locking v-status
+    (cond
+      :let [status @v-status]
+      (= status :intializing) -1
+      :let [^Timestamps timestamps @a-timestamps
+            last-improvement-time (.-last-improvement-time timestamps)
+            stop-time (.-stop-time timestamps)]
+      (= last-improvement-time -1) (get-runtime search)
+      (or (= status :idle) (= status :disposed))
+      (- stop-time last-improvement-time)
+      :else (- (System/currentTimeMillis) last-improvement-time))))
 
-(defn get-steps-without-improvement ^long [^SearchState ss]
+(defn get-steps-without-improvement
+  ^long [{:keys [a-step-info v-status] :as search}]
   (cond
-    :let [status (.-status ss)
-          steps-since-last-improvement (.-steps-since-last-improvement ss)]
+    :let [status @v-status]
     (= status :initializing) -1
-    (= steps-since-last-improvement -1) (.-current-steps ss)
+    :let [^StepInfo @a-step-info
+          steps-since-last-improvement (.-steps-since-last-improvement step-info)]
+    (= steps-since-last-improvement -1) (.-current-steps )
     :else steps-since-last-improvement))
 
-(defnc compute-delta [^Search search current-evaluation previous-evaluation]
-  :let [problem (.-problem search)]
-  (prob/minimizing? problem) (- (mrp/value previous-evaluation) (mrp/value current-evaluation))
-  :else (- (mrp/value current-evaluation) (mrp/value previous-evaluation)))
+(defn compute-delta ^double [^Search search current-evaluation previous-evaluation]
+  (cond
+    :let [problem (.-problem search)]
+    (prob/minimizing? problem)
+    (- (mrp/value previous-evaluation) (mrp/value current-evaluation)),
+    :else (- (mrp/value current-evaluation) (mrp/value previous-evaluation))))
 
 ;; Search callbacks
 
-(defn search-started [^Search search]
+(defn search-started [{:keys [a-timestamps a-min-delta a-step-info] :as search}]
   ;; Initialize search by calling init ?
-  (let [state (.-state search)
-        start-time (System/currentTimeMillis),
-        stop-time -1
-        current-steps 0
-        last-improvement-time -1
-        steps-since-last-improvement -1
-        min-delta -1]
-    (swap!
-     state
-     (fn [^SearchState ss]
-       (let [best-solution (.-best-solution ss),
-             best-solution-evaluation (.-best-solution-evaluation ss)
-             best-solution-validation (.-best-solution-validation ss)
-             status (.-status ss)]
-         (SearchState. start-time stop-time
-                       current-steps last-improvement-time
-                       steps-since-last-improvement false
-                       min-delta best-solution best-solution-evaluation
-                       best-solution-validation ss))))))
+  ;; (init search)
+  (reset! a-timestamps (Timestamps. (System/currentTimeMillis) -1 -1))
+  (reset! a-step-info (StepInfo. 0 -1))
+  (reset! min-delta -1))
 
 (defn search-stopped [^Search search]
   (swap! (.-state search) assoc :stop-time (System/currentTimeMillis)))
 
-(defn search-step [^Search search])
+(defn search-step [search])
 
-(defn search-disposed [^Search search])
+(defn search-disposed [search])
+
+(defn init [search])
 
 
 
